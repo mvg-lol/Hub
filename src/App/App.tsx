fd38c87d3f39aa7b65d968270c482ad22478ca8a
marto <martinho.tavares@live.com.pt>
2024-06-23 00:24:59 +0100

    react + vite + threejs + router

---
+++ src/App/App.tsx
@@ -0,0 +1,48 @@
+import { memo } from 'react';
+import './App.css'
+
+function alterarBG() {
+  const num: number = Math.floor(Math.random() * 100080000)
+  if (num <= 772001 && num >= 0){
+    document.getElementsByTagName("body").item(0)!.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1164552263906377749/1253707209859141663/2Q.png?ex=667826d3&is=6676d553&hm=1c19d01ed7d7a0e849e77a9819c072f199f6f9454d1ae0cefb5581ad906b2bb4&')"
+  }
+}
+
+function AppUnMemo() {
+  const lineStyle: React.CSSProperties = {borderTop:"2px solid black"};
+  alterarBG();
+  return (
+      <div style={{backgroundImage: "/frigorifico.png"}}>
+        <br/>
+        <h1>
+          <a style={{color:"goldenrod"}} href="https://github.com/mvg-lol">MVG</a>
+        </h1>
+        <br/>
+        <br/>
+        <hr style={lineStyle}/>
+        <h2>
+          <a style={{color:"#d60270"}} href="https://rabosgigantes.com">PeidosFTW</a>
+        </h2>
+        <br/>
+        <h2>
+          <a style={{color:"#d60270"}} href="https://mrbrodinha.com">MrBrodinha</a>
+        </h2>
+        <br/>
+        <h2>
+          <a style={{color:"#9b4f96"}} href="https://ricadinho.eu">RICADINHO</a>
+        </h2>
+        <br/>
+        <h2>
+          <a style={{color:"#0038a8"}} href="https://pereira3.github.io/">Pereira ッ</a>
+        </h2>
+        <br/>
+        <h2>
+          <a style={{color:"#0038a8"}} href="https://www.youtube.com/watch?v=Ec4YbVP9R-A">Quim</a>
+        </h2>
+        <br/>
+        <hr style={lineStyle}/>
+      </div>
+  )
+}
+const App = memo(AppUnMemo) //stateless component, memo faz com que ele n de rerender
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import App from './App/App.tsx'
+import Three from './Threejs/Threejs.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/three",
+    element: <Three/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
