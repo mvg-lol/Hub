8bd5c6459e3ba6edefe06cbc37ffcf0c82b1491a
marto <martinho.tavares@live.com.pt>
2024-09-11 16:58:56 +0100

    teste router

--- joguinhos/src/main.tsx
+++ joguinhos/src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Joguinhos from './Joguinhos.tsx'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+import Three from './Threejs/Threejs.tsx'
+
+const router = createHashRouter([
+  {
+    path: '/joguinhos',
+    element:<Joguinhos/>
+  },
+  {
+    path: '/joguinhos/cubo',
+    element: <Three/>
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Joguinhos />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
